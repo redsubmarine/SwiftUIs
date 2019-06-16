043958babad2f6deadc68ea8fcc823c0f19830b8
red <redsubmarine@yahoo.com>
2019-06-16 22:57:29 +0900

    add FetchJsonAndImagesDemo Project
    for Lets Build That App's Youtube
    https://www.youtube.com/watch?v=xT4wGOc2jd4&t=32s

---
+++ FetchJsonAndImagesDemo/FetchJsonAndImagesDemo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  FetchJsonAndImagesDemo
+//
+//  Created by 양원석 on 2019/06/16.
+//  Copyright © 2019 red. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
